cf4b4bfdf00231c17901835ec7d3703e7bc56bda
Raphael Cerqueira <raphaelfc@gmail.com>
2021-07-02 18:34:24 -0300

    Initial Commit

---
+++ AppOnboardingDesign/AppOnboardingDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppOnboardingDesignApp.swift
+//  AppOnboardingDesign
+//
+//  Created by Raphael Cerqueira on 02/07/21.
+//
+
+import SwiftUI
+
+@main
+struct AppOnboardingDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
